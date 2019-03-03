83a42cdb349177bcd7a33bc4dd89877948fc2302
liyifan <937253559@qq.com>
2019-03-03 11:05:45 +0800

    Initial commit

---
+++ test.py
@@ -0,0 +1,26 @@
+import tensorflow as tf
+
+# a = tf.constant([1.0, 2.0], name="a")
+# b = tf.constant([2.0, 3.0], name="b")
+#
+# retsult = a + b
+#
+# print(a.graph is tf.get_default_graph())
+
+g1 = tf.Graph()
+with g1.as_default():
+    v = tf.get_variable("v", initializer=tf.zeros_initializer()(shape=[1]))
+
+g2 = tf.Graph()
+with g2.as_default():
+    v = tf.get_variable("v", initializer=tf.ones_initializer()(shape=[1]))
+
+with tf.Session(graph=g1) as sess:
+    tf.global_variables_initializer().run()
+    with tf.variable_scope("", reuse=True):
+        print(sess.run(tf.get_variable("v")))
+
+with tf.Session(graph=g2) as sess:
+    tf.global_variables_initializer().run()
+    with tf.variable_scope("", reuse=True):
+        print(sess.run(tf.get_variable("v")))
